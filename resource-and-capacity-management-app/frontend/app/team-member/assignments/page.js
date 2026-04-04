725fa511245cb32cdb5223d42bd6fabea14c494c
Jack <Jackson.battista02@gmail.com>
2026-04-04 09:14:29 -0600

    dynamic change

--- resource-and-capacity-management-app/frontend/app/team-member/assignments/page.js
+++ resource-and-capacity-management-app/frontend/app/team-member/assignments/page.js
@@ -1,7 +1,7 @@
-export const dynamic = 'force-dynamic';
+
 "use client";
 
-
+export const dynamic = 'force-dynamic';
 /* =============================================================================
    TeamMemberAssignmentsPage.jsx
    -----------------------------------------------------------------------------
