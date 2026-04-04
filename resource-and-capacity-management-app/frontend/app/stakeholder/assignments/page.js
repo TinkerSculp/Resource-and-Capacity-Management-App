3f34260ac090c8fcd7fb20d7d6ac81c09490c2d4
Jack <Jackson.battista02@gmail.com>
2026-04-04 09:09:05 -0600

    changes

--- resource-and-capacity-management-app/frontend/app/stakeholder/assignments/page.js
+++ resource-and-capacity-management-app/frontend/app/stakeholder/assignments/page.js
@@ -1,5 +1,6 @@
-"use client";
 export const dynamic = 'force-dynamic';
+"use client";
+
 
 /* =============================================================================
    StakeholderAssignmentsPage.jsx
